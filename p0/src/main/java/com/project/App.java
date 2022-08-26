837452747fe8418be17b36e213eba160d536a32c
youmarconan <youmarconan@gmail.com>
2022-08-26 10:54:17 -0700

    connect to the database

--- p0/src/main/java/com/project/App.java
+++ p0/src/main/java/com/project/App.java
@@ -1,9 +1,41 @@
 package com.project;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) 
     {
-        System.out.println( "Hello World!" );
+        
+        
+        try{
+        Properties prop = new Properties();
+
+        prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("application.properties"));
+
+
+        Connection conn = DriverManager.getConnection(prop.getProperty("url"),prop.getProperty("username") ,prop.getProperty("password") );
+       
+        
+
+        if (conn != null){
+        System.out.println("Successful connection!");
+        }
+        conn.close();
+
+        }
+        catch(IOException e){
+           throw new RuntimeException("Could not read from properties file.", e);
+        }
+        
+        catch(SQLException e){
+            System.out.println("Unable to connect to the database!");
+        } 
+        
+        
     }
 }
